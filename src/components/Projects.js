801a77f616b0664ef6e561bedc3623fcab2ceb00
Elvis Odhiambo <ochiengelvis90@gmail.com>
2024-08-09 17:47:17 +0300

    grid design

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -14,19 +14,19 @@ const Projects = () => {
       <h1 className='text-5xl sm:text-3xl lg:text-7xl font-bold py-4' id="hero_name">Projects</h1>
 
       <section className='px-8 lg:px-20'>
-        <div className='flex flex-col lg:flex-row justify-center items-center'>
-          <div className='px-12'>
+        <div className='grid grid-cols-1 lg:grid-cols-2'>
+          <div className='p-12'>
             <h1 className='text-3xl lg:text-5xl'>Project <span>Evolve1</span></h1>
             <p>Ante at sapien dapibus, luctus euismod fames. Quam vehicula erat rutrum nunc porttitor nam. Cras sodales natoque fermentum eleifend neque quam etiam vel auctor. Magnis habitant lobortis cursus integer porta congue tempus cras pretium. Fringilla sapien lacus consectetur massa conubia nunc himenaeos duis. Platea dignissim scelerisque sapien magna malesuada iaculis mauris morbi.</p>
           </div>
 
-          <img src={Img1} className='' style={{ width: "50%" }} />
+          <img src={Img1} className='' style={{ width: "50vw" }} />
         </div>
 
-        <div className='flex flex-col lg:flex-row justify-center items-center'>
-          <img src={Img2} className='' style={{ width: "50%" }} />
+        <div className='grid grid-cols-1 lg:grid-cols-2'>
+          <img src={Img2} className='' style={{ width: "50vw" }} />
 
-          <div className='px-12'>
+          <div className='p-12'>
             <h1 className='text-3xl lg:text-5xl'>Project <span>Evolve2</span></h1>
             <p>Ante at sapien dapibus, luctus euismod fames. Quam vehicula erat rutrum nunc porttitor nam. Cras sodales natoque fermentum eleifend neque quam etiam vel auctor. Magnis habitant lobortis cursus integer porta congue tempus cras pretium. Fringilla sapien lacus consectetur massa conubia nunc himenaeos duis. Platea dignissim scelerisque sapien magna malesuada iaculis mauris morbi.</p>
           </div>
